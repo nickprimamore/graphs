becc88ef82a67c581f2d498f9d97419f17a5d498
nickprimamore <lancernp@gmail.com>
2018-03-02 11:36:27 -0500

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,57 @@
+#include "graph.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <math.h>
+
+#define TOTAL_TESTS 3
+
+bool run_test(int test_number) {
+    switch(test_number) {
+        case 1: {
+            // test01 :
+            break;
+        }
+            
+        case 2: {
+            // test02
+            break;
+        }
+            
+        case 3: {
+            // test03 :
+            break;
+        }
+            
+            /*
+             case 4: {
+             // test04 : (description here)
+             (implementation here)
+             break; // if your test suceeds, break
+             }
+             */
+            
+        default: {
+            printf("ERROR! Test number %d was not found!\n", test_number);
+            exit(1);
+        }
+            
+    }
+    // test must have passed.
+    return true;
+}
+
+int main() {
+    int num_fail = 0;
+    for (int tn = 1; tn <= TOTAL_TESTS; tn++) {
+        printf("- running test # %02d ... ", tn);
+        int result = run_test(tn);
+        printf("%s\n", (result == true ? "succeed" : "FAIL"));
+        if (result == false) num_fail++;
+    }
+    printf("SUMMARY: %d FAIL, %d succeed\n", num_fail, TOTAL_TESTS - num_fail);
+}
+
+
+
