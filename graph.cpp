9cea67e45c56ffcf741480211fe90cd8ff9de54c
nickprimamore <lancernp@gmail.com>
2018-03-02 12:16:58 -0500

    Partially implemented Node, Graph constructor, and numVerts()

--- graph.cpp
+++ graph.cpp
@@ -12,3 +12,46 @@
 #include <math.h>
 using namespace std;
 
+//implementing Node defined in graph.h for adjacency list
+struct Graph :: Node{
+    int value;
+    float edgeWeight;
+    Node* next;
+}
+
+/* Make an empty graph with n vertices (empty means all edges either set to INFINITY or NULL,
+ depending on representation), using either adjacency matrix
+ or adjacency lists depending on whether rep==MATRIX or rep==LIST.
+ if n <1, return NULL
+ if rep is not MATRIX or LIST, return NULL
+ */
+Graph :: Graph(int n, int rep){
+    if(n < 1 || rep != MATRIX || rep!= LIST) return NULL;
+    
+    if(rep == LIST){
+        numVertices = n;
+        type = LIST;
+        Graph::Node* listArray[numVertices];
+        list = listArray;
+        //need to set all edges to infinity or null
+    }
+    else{
+        numVertices = n;
+        type = MATRIX;
+        float matrixArray[numVertices * numVertices]; // 1D arrray implementation
+        matrix = matrixArray;
+        //need to set all edges to infinity or null
+    }
+}
+
+/* free the graph object and ALL its resources.
+ */
+Graph :: ~Graph(){
+    
+}
+
+/* return number of vertices.
+ Note that this is needed since the property numVerts is private*/
+int Graph :: numVerts(){
+    return numVertices;
+}
